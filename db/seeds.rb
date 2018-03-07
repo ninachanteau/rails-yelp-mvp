a043738ef74ca5bdbead455497063e3d7f03b493
Nina Chanteau <nina.chanteau@hec.edu>
2018-03-07 12:28:12 +0100

    create the controllers and the views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Vivant',
+    address: 'Paris, 10',
+    phone_number: '0153624435',
+    category: 'french'
+  },
+  {
+    name: 'Atelier Joel Robuchon',
+    address: 'Paris, 07',
+    phone_number: '0176908453',
+    category: 'french'
+  },
+  {
+    name: 'Toyo',
+    address: 'Paris, 14',
+    phone_number: '0135277689',
+    category: 'japanese'
+  },
+  {
+    name: 'Ober Mamma',
+    address: 'Paris, 11',
+    phone_number: '0133255987',
+    category: 'italian'
+  },
+  {
+    name: 'Sushi Samba',
+    address: 'London, Liverpool Street',
+    phone_number: '0864537298',
+    category: 'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
